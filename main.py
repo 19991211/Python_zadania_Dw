a77c2e8326d97d9f6d4c3638c2fb84a30fd17937
Damian <d_wtyklo@o2.pl>
2022-02-26 10:58:53 +0100

    Dodanie kolejnych zmiennych

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import sys
 
 print(sys.version)
 print("hello World!!!")
-a = 'isi grupa niestacjonarna'
+a = 'isi\ngrupa niestacjonarna'
 print(a)
 print(type(a))
 
+
+b = 5
+print(b)
+
+c = 5.4
+print(c)
+
+d = 3+2j
+print(d)
+print(type(d))
 
 
 
