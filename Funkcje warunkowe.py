a01542383637f84e6fee03b2ddc40b1e2d947f3b
Damian <d_wtyklo@o2.pl>
2022-02-27 13:36:40 +0100

    Dodanie zmiennych

--- Funkcje warunkowe.py
+++ Funkcje warunkowe.py
@@ -9,11 +9,27 @@
 # x<t
 # x>=y
 # x<=y
-a = 7
-b = 5
-if a > b:
-    print('liczba a jest większa od liczby b')
-elif a < b:
-    print('liczba a jest mniejsza od b')
+# a = 7
+# b = 5
+# if a > b:
+#     print('liczba a jest większa od liczby b')
+# elif a < b:
+#     print('liczba a jest mniejsza od b')
+# else:
+#    print('liczby sa równe')
+a = input("Podaj liczbe a: ")
+b = input("podaj liczbe b: ")
+c = input("Podaj liczbe c: ")
+d = input("podaj liczbe d: ")
+#     print(a)
+#     print(type(a))
+#      a = int(a)
+#     print(type(a))
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+if ( a > b ) & ( c > d ):
+    print('liczba a jest wieksza od liczby b i liczba c jest wieksza od liczby d')
 else:
-    print('liczby sa równe')
+    print('liczba a jest mniejsza niz liczba b lub liczba c jest mniejsza od liczby d')
