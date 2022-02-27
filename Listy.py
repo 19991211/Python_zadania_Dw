a8bf38ca609d286e55d44d0359fa404953900079
Damian <d_wtyklo@o2.pl>
2022-02-27 14:43:21 +0100

    Dodawanie usuwanie slownik lista

---
+++ Listy.py
@@ -0,0 +1,12 @@
+#lista=[] przyjmie prawie wszystko
+#słownik={}el postaci
+# klucz: wartosc <---- element słownika
+lista=[1,2,3]
+slownik = {'klucz1':1, 'klucz2':2, 'klucz3':3}
+print(slownik)
+del slownik['klucz1']
+print(slownik)
+del lista[0]
+print(lista)
+
+
