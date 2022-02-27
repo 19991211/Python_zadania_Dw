aafa383242bc268646906b45c6d1fcac00b0cdc5
Damian <d_wtyklo@o2.pl>
2022-02-27 14:29:44 +0100

    Dodanie funkcji

--- funkcje Try.py
+++ funkcje Try.py
@@ -1,4 +1,19 @@
-#try
+#try:
     #blok instrukcji
-#except nazwa_błedu
+#except nazwa_błedu:
     #blok instrukcji po napotkaniu błedu
+#else:
+    #instrukcje bez błedu
+liczba1 = input('Podaj pierwszą liczbę: ')
+liczba2 = input('Podaj drugą liczbę: ')
+
+
+try:
+    liczba1 = int(liczba1)
+    liczba2 = int(liczba2)
+    wynik = liczba1/liczba2
+    print('Wynik działania = ' + str(wynik))
+except ZeroDivisionError:
+    print('Nie można dzielić przez 0')
+except ValueError:
+    print('Nie wczytano liczby całkowitej')
