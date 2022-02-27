b3455e2aef24730a67c5b2720eb5656487b9c634
Damian <d_wtyklo@o2.pl>
2022-02-27 14:12:44 +0100

    Dodanie break

--- petle_while.py
+++ petle_while.py
@@ -3,3 +3,22 @@
 # instrukcja lub blok instrukcji
 #else:
 #   instrukcja po zakończeniu działania pętli
+
+
+# z = 0
+# while z != 11:
+#     print(z)
+#     z += 1
+# else:
+#     print('wyświetlono ' + str(z) + ' liczb')
+
+lista = [4,6,2,3,5,9,1]
+liczba = input('podaj liczbe: ')
+licznik = 0
+while licznik != len(lista):
+    if int(liczba) - lista[licznik] == 0:
+        print(liczba + '- ' + str(lista[licznik]) + ' = 0')
+        break
+    else:
+        licznik += 1
+
