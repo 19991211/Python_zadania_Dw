53496af613545e394edf4dd52824381b4d901295
Damian <d_wtyklo@o2.pl>
2022-02-27 14:54:56 +0100

    zmiana wartosci listy

--- Listy.py
+++ Listy.py
@@ -4,9 +4,15 @@
 lista=[1,2,3]
 slownik = {'klucz1':1, 'klucz2':2, 'klucz3':3}
 print(slownik)
-del slownik['klucz1']
+del slownik['klucz1'] #<----- usuwanie pierwszego klucza z słownika
 print(slownik)
-del lista[0]
+del lista[0] #<---- usuwanie pierwszego indexu listy
 print(lista)
+slownik.popitem()
+slownik['klucz2'] = 'samochód' #<----- zmiana wartości klucza
+print(slownik)
+lista[1] = 'Opel' #zmiana wartosci
+print(lista)
+
 
 
