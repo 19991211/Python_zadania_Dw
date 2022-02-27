33c68d9ecb20006fc9ba199d3df6d456f9295739
Damian <d_wtyklo@o2.pl>
2022-02-27 14:20:12 +0100

    dwie petle for

--- petle_while.py
+++ petle_while.py
@@ -12,13 +12,21 @@
 # else:
 #     print('wyświetlono ' + str(z) + ' liczb')
 
-lista = [4,6,2,3,5,9,1]
-liczba = input('podaj liczbe: ')
-licznik = 0
-while licznik != len(lista):
-    if int(liczba) - lista[licznik] == 0:
-        print(liczba + '- ' + str(lista[licznik]) + ' = 0')
-        break
-    else:
-        licznik += 1
+# lista = [4,6,2,3,5,9,1]
+# liczba = input('podaj liczbe: ')
+# licznik = 0
+# while licznik != len(lista):
+#     if int(liczba) - lista[licznik] == 0:
+#         print(liczba + '- ' + str(lista[licznik]) + ' = 0')
+#         break
+#     else:
+#         licznik += 1
 
+lista1 = [4,6,2,3,5,9,1]
+lista2 = [7,3,4,6]
+suma = []
+for a in lista1:
+    for b in lista2:
+        wynik = a + b
+        suma.append(wynik)
+    print(suma)
