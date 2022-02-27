d591cf32d91b6bfe139f918624822155a23c9c9c
Damian <d_wtyklo@o2.pl>
2022-02-27 13:46:49 +0100

    Petla for

--- Petle_for.py
+++ Petle_for.py
@@ -4,3 +4,15 @@
         #instrukcje po zakkonczeniu pętli
 
     #range(start,stop,step)
+    #for(int i=0;i<10;i++)
+
+
+# for i in range(0, 6, 1):
+#     print(i)
+
+lista = ['cos', 3, 4, 5.6]
+for elemnet in lista:
+    print(elemnet)
+else:
+    print('wyświetlono wszystkie elementy listy')
+
