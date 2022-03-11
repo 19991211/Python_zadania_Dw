4c6a7375d86b76eaebf59ed82026046d880d1e1f
Damian <d_wtyklo@o2.pl>
2022-03-11 20:30:39 +0100

    Zadania LAB 1

---
+++ Zadania Lab1.py
@@ -0,0 +1,31 @@
+#zad 1
+a = 'Tolek '
+b = 'Banan'
+print(a+b)
+c =2
+print(c)
+d =100
+print(d)
+e = 0.2
+print(e)
+f = 99.99
+print(f)
+g = 3+2j
+print(g)
+h = 10 + 2j
+print(h)
+#zad 2
+a = int(input("Podaj liczbe"))
+b = int(input("Podaj drugą liczbe"))
+operacja = input("Podaj działanie matematyczne +,-,*,/")
+print(operacja)
+
+if (operacja == "+"):
+    print("wynik dodawania",a+b)
+if (operacja == "-"):
+    print("Wynik odejmowania",a-b)
+if (operacja == "*"):
+    print("Wynik mnożenia",a*b)
+if (operacja == "/"):
+    print("wynik dzielenia",a/b)
+#zad 3
